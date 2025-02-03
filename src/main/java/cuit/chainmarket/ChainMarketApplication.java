70f744530f6cecf92b4d7128e476e768a195ed1d
root <2059997196@qq.com>
2025-02-03 14:07:39 +0800

    init of project

---
+++ src/main/java/cuit/chainmarket/ChainMarketApplication.java
@@ -0,0 +1,13 @@
+package cuit.chainmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cuit/chainmarket/ChainMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cuit.chainmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
